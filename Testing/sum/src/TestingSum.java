6f6cff6fca678d81a57ea97f24f7776af5c78750
Yeasmin Akter Elma <112389827+Yeasminakterelma77@users.noreply.github.com>
2022-09-30 20:42:54 +0600

    first commit

---
+++ Testing/sum/src/TestingSum.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TestingSum {
+    
+
+@Test
+    public void TestSum01(){
+        App app = new App();
+        double actual = 2.2679331;
+        assertEquals(app.calculate(10, 10),actual,actual);
+
+
+    }
+}
